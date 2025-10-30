59c5f14be2a850024a59f0184c7b60447265deba
a2r2 <ardroid.attacck@gmail.com>
2025-10-30 16:40:32 -0600

    some changes of the couritines

--- app/src/main/java/com/example/exercise/MainActivity.kt
+++ app/src/main/java/com/example/exercise/MainActivity.kt
@@ -28,6 +28,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var adapter: MovieAdapter
     private lateinit var adapter2: ItemImageAdapter
 
+    val moviesall = mutableListOf<Movie>()
+
     private lateinit var imageViewExample: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -49,35 +51,74 @@ class MainActivity : AppCompatActivity() {
 
         fun getMovies() {
             val client = OkHttpClient()
-            val request = Request.Builder()
-                .url("https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1&api_key=55957fcf3ba81b137f8fc01ac5a31fb5")
-                .build()
+            val allMovies = mutableListOf<Movie>()
+            val totalPages = 10
 
             CoroutineScope(Dispatchers.IO).launch {
                 try {
-                    client.newCall(request).execute().use { response ->
-                        val json = JSONObject(response.body()!!.string())
-                        val results = json.getJSONArray("results")
-                        val movies = mutableListOf<Movie>()
-                        for (i in 0 until results.length()) {
-                            val movieJson = results.getJSONObject(i)
-                            val title = movieJson.getString("title")
-                            val overview = movieJson.getString("overview")
-                            val pick = movieJson.getString("poster_path")
-                            val language = movieJson.getString("original_language")
-                            val dateRealese = movieJson.getString("release_date")
-                            val movie = Movie(i, title, overview, pick, language, dateRealese)
-                            movies.add(movie)
-                        }
+                    for (page in 1..totalPages) {
+                        val request = Request.Builder()
+                            .url("https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=$page&api_key=55957fcf3ba81b137f8fc01ac5a31fb5")
+                            .build()
 
-                        withContext(Dispatchers.Main) {
-                            for (movie in movies) {
-                                adapter.add(movie)
-                                val imageView = ImageView(this@MainActivity)
-                                loadImageIntoImageView(imageView, "https://image.tmdb.org/t/p/w500/${movie.poster_path}")
-                                findViewById<LinearLayout>(R.id.listViewContent).addView(imageView)
+                        client.newCall(request).execute().use { response ->
+                            val json = JSONObject(response.body()!!.string())
+                            val results = json.getJSONArray("results")
+
+                            for (i in 0 until results.length()) {
+                                val movieJson = results.getJSONObject(i)
+                                val title = movieJson.getString("title")
+                                val overview = movieJson.getString("overview")
+                                val pick = movieJson.getString("poster_path")
+                                val language = movieJson.getString("original_language")
+                                val dateRealese = movieJson.getString("release_date")
+                                val movie = Movie(i, title, overview, pick, language, dateRealese)
+                                allMovies.add(movie)
                             }
                         }
+                    }
+
+                    withContext(Dispatchers.Main) {
+                        moviesall.clear()
+                        moviesall.addAll(allMovies)
+
+                        val filteredMovies = moviesall.filter { it.original_language == "es" }
+
+                        val filteredMoviessecond = moviesall.filter { it.original_language == "en" }
+                        adapter = MovieAdapter(this@MainActivity, ArrayList(filteredMovies))
+
+                        adapter = MovieAdapter(this@MainActivity, ArrayList(filteredMoviessecond))
+                        listView.adapter = adapter
+
+                        for (movie in filteredMovies) {
+                            val imageView = ImageView(this@MainActivity)
+
+                            // Cargar imagen
+                            loadImageIntoImageView(imageView, "https://image.tmdb.org/t/p/w500/${movie.poster_path}")
+
+                            // Guardar el objeto Movie como tag en el ImageView
+                            imageView.tag = movie
+
+                            // Asignar evento onClick
+                            imageView.setOnClickListener {
+                                val clickedMovie = it.tag as Movie
+
+                                val dialogImage = ImageView(this@MainActivity)
+                                loadImageIntoImageView(dialogImage, "https://image.tmdb.org/t/p/w500/${clickedMovie.poster_path}")
+
+                                AlertDialog.Builder(this@MainActivity)
+                                    .setTitle(clickedMovie.title)
+                                    .setCustomTitle(dialogImage)
+                                    .setMessage("Language: ${clickedMovie.original_language}, " +
+                                            "Date: ${clickedMovie.release_date}, " +
+                                            "Overview: ${clickedMovie.overview}")
+                                    .setPositiveButton("Entendido", null)
+                                    .show()
+                            }
+
+                            findViewById<LinearLayout>(R.id.listViewContent).addView(imageView)
+                        }
+
                     }
                 } catch (e: Exception) {
                     e.printStackTrace()
@@ -91,7 +132,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
        listView = findViewById(R.id.listView)
         listView.dividerHeight = 9
+
+
+
         adapter = MovieAdapter(this, ArrayList())
+//println("kkkkkkkkkkkkkkkkkkkkk ${}")
 
         listView.onItemClickListener = AdapterView.OnItemClickListener { _, _, position, _ ->
             val selectedMovie = adapter.getItem(position) as Movie
@@ -102,33 +147,22 @@ class MainActivity : AppCompatActivity() {
             AlertDialog.Builder(this)
                 .setTitle(selectedMovie.title)
                 .setCustomTitle(imageView)
-                .setMessage("Language: ${ selectedMovie.original_language }, Date: ${ selectedMovie.release_date } ,Overview: ${ selectedMovie.overview }")
+                .setMessage("Language: ${ selectedMovie.original_language }, " +
+                        "Date: ${ selectedMovie.release_date } ," +
+                        "Overview: ${ selectedMovie.overview }")
                 .setPositiveButton("Entendido", null)
                 .show()
         }
 
 
-        listView.adapter = adapter
+        //listView22.onItemClickListener
+
+
+      //  listView.adapter = adapter
 
       getMovies()
 
-        val apiKey = "55957fcf3ba81b137f8fc01ac5a31fb5"
-        val call = ApiClient.tmdbService.getNowPlaying(apiKey, "en-US", 1)
-        call.enqueue(object : Callback<MoviesResponse> {
-            override fun onResponse(call: Call<MoviesResponse>, response: Response<MoviesResponse>) {
-                val movies = response.body()?.results
 
-                if (movies != null) {
-                    println("movies"+movies)
-                }
-                // Use the movies list
-            }
-
-            override fun onFailure(call: Call<MoviesResponse>, t: Throwable) {
-                // Handle error
-            }
-
-        })
     }
 }
 
